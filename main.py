af55cb3d79f7f47a63a56535c33b88a063f413c9
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2023-01-05 01:16:08 -0500

    add undock, locate mine, travel to, mine ore, and loop for full

--- main.py
+++ main.py
@@ -24,19 +24,64 @@ with open('resources/keybinding.yaml', 'r') as yamlfile:
 
 
 
-# function screenshot creation
+# function screenshot creation and run code
 def createScreenshot():
     # take screenshot and save it
     image = pyautogui.screenshot(region = (0, 0, screenWidth,screenHeight+25 ))
     image.save(r"C:\Users\MonkeyKing 2\Desktop\AutoMiningEVE\screenshots\1.png")
 
     #cv2 process image
-    image = cv2.cvtColor(numpy.array(image),0)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # image = cv2.cvtColor(numpy.array(image),0)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # pos returns coordinates left top width height, find match with confidence 0.5
-    pos = pyautogui.locateOnScreen("resources/undock.png", grayscale=True, confidence=.5)
+    # pos = pyautogui.locateOnScreen("resources/undock.png", grayscale=True, confidence=.5)
+    # print(pos)
+    # pyautogui.moveTo(pos[0]+pos[2]/2, pos[1]+pos[3]/2)
+    # pyautogui.click()
+
+    time.sleep(5)
+    
+    # open location
+    # pyautogui.keyDown('l')
+    # time.sleep(1)
+    # pos = pyautogui.locateOnScreen("resources/miningLocation.png", grayscale=True, confidence=0.5)
+    # print(pos)
+    # pyautogui.moveTo(pos[0]+pos[2]/2, pos[1]+pos[3]/2)
+    # pyautogui.rightClick()
+
+    # # warp to location
+    # pos = pyautogui.locateOnScreen("resources/warpToLocation.png", grayscale=True, confidence=0.5)
+    # print(pos)
+    # pyautogui.moveTo(pos[0]+pos[2]/2, pos[1]+pos[3]/2)
+    # pyautogui.click()
+
+    # time.sleep(20)
+
+
+
+    # start mining
+    pos = pyautogui.locateOnScreen("resources/closestOre.png", grayscale=True, confidence=0.3)
     print(pos)
+    pyautogui.moveTo(pos[0]+pos[2]/2, pos[1]+pos[3]/2+30)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.hotkey('ctrl')
+    time.sleep(3)
+    pyautogui.hotkey('f2')
+
+
+    # wait until bag full
+ 
+    while(1):
+        time.sleep(0.5)
+        pos = pyautogui.locateOnScreen("resources/CargoFull.png", grayscale=True, confidence=0.3)
+        print(pos)
+        if (pos != 'None'):
+            print(strftime("%H:%M:%S", gmtime()), "Full")
+        break
+
+    
 
 
 
@@ -45,7 +90,7 @@ def createScreenshot():
 
 
 # run script
-time.sleep(5)
+time.sleep(2)
 print(strftime("%H:%M:%S", gmtime())),
 createScreenshot()
 
